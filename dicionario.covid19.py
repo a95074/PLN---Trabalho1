b0f9330d02e919d2b0f55f4d97f22646a95b3360
a95074 <92465292+a95074@users.noreply.github.com>
2025-04-09 22:17:37 +0100

    limpezas

--- dicionario.covid19.py
+++ dicionario.covid19.py
@@ -3,7 +3,21 @@ import json
 
 filename = "diccionari-multilinguee-de-la-covid-19.xml"
 
-#primeiro passo foi retirar manualmente no xml as páginas de introdução que não continham 
+#primeiro passo foi retirar manualmente no xml as páginas de introdução que não continham informação relevante para o json
+#foi retirado manualmente os indexes e bibliografias
 
 with open(filename, 'r', encoding='utf-8') as file:
-    ficheiro = file.read()
+    texto = file.read()
+
+texto = re.sub(r'</page>\s*', '', texto) #remove todas as trocas de página do ficheiro
+texto = re.sub(r'<page number="\d+" position="absolute" top="0" left="0" height="1190" width="914">\s*\n?', '', texto) #remove os títulos das páginas e faz com que não fique uma linha em branco entre elas
+texto = re.sub(r'<fontspec id="\d+" size="\d+" family="[^"]+" color="#[0-9a-fA-F]{6}"\s*/>*\n?', '', texto) #remove infos da pagina
+texto = re.sub(r'<text top="\d+" left="\d+" width="\d+" height="\d+" font="\d+">\d+\s?</text>*\n?', '', texto) #remove o número da definição
+texto = re.sub(r'<text top="\d+" left="\d+" width="\d+" height="\d+" font="\d+"><i> n m</i></text>\n?', '', texto) #remove os n m
+texto = re.sub(r'<text top="\d+" left="\d+" width="\d+" height="\d+" font="\d+"><i> n f</i></text>', '', texto) #remove os n 
+texto = re.sub(r'<text top="\d+" left="\d+" width="\d+" height="\d+" font="\d+"><i> n</i></text>', '', texto)
+
+
+
+with open("teste.xml", 'w', encoding='utf-8') as novo_arquivo:
+    novo_arquivo.write(texto)
